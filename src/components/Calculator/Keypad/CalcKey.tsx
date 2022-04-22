1e2c4e313cf8a49926aa1e56594caa4faf17395a
andy-devs <markushin.andrew@gmail.com>
2022-04-22 17:17:52 +0600

    Keys Component

--- src/components/Calculator/Keypad/CalcKey.tsx
+++ src/components/Calculator/Keypad/CalcKey.tsx
@@ -5,7 +5,19 @@ interface Props {
 }
 
 const CalcKey = ({ value }: Props) => {
-	return <button>{value}</button>;
+	let keyClasses = classes.calc__key;
+
+	if (value === 'DEL') {
+		keyClasses += ' ' + classes['del-key'];
+	}
+	if (value === 'RESET') {
+		keyClasses += ' ' + classes['reset-key'];
+	}
+	if (value === '=') {
+		keyClasses += ' ' + classes['equal-key'];
+	}
+
+	return <button className={keyClasses}>{value}</button>;
 };
 
 export default CalcKey;
--- src/components/Calculator/Keypad/CalcKeypad.tsx
+++ src/components/Calculator/Keypad/CalcKeypad.tsx
@@ -2,8 +2,35 @@ import classes from './CalcKeypad.module.css';
 
 import CalcKey from './CalcKey';
 
+const keys = [
+	'7',
+	'8',
+	'9',
+	'DEL',
+	'4',
+	'5',
+	'6',
+	'+',
+	'1',
+	'2',
+	'3',
+	'-',
+	'.',
+	'0',
+	'/',
+	'x',
+	'RESET',
+	'=',
+];
+
 const CalcKeypad = () => {
-	return <div className={classes.calc__keypad}></div>;
+	return (
+		<div className={classes.calc__keypad}>
+			{keys.map((key) => (
+				<CalcKey value={key} />
+			))}
+		</div>
+	);
 };
 
 export default CalcKeypad;
